1531007cc68c65a66bb75c9fe72c1a89587fca27
jdizhur-rgb <j.dizhur@gmail.com>
2026-09-06 15:11:24 -0400

    Minimal Streamlit smoke test for blank screen

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="Vet Cancer Treatment Finder", page_icon="🐾", layout="wide")
-
-PAGES = [
-    st.Page("pages/1_Clinical_Trial_Finder.py", title="Clinical Trial Finder", icon="🐾", default=True),
-    st.Page("pages/2_Additional_Oncology_Options.py", title="Additional Oncology Options", icon="🧬"),
-]
-
-page = st.navigation(PAGES, position="hidden")
-page.run()
+st.set_page_config(page_title="Vet Cancer Treatment Finder", page_icon="🐾")
+st.title("TEST")
+st.write("Streamlit deployment is running.")
