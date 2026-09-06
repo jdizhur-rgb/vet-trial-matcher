ca0ce739f0e81c9ae272c2e5ee65d2a9c68f6aec
jdizhur-rgb <j.dizhur@gmail.com>
2026-09-06 15:12:14 -0400

    Test direct execution of Clinical Trial Finder page

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import streamlit as st
+import runpy
 
-st.set_page_config(page_title="Vet Cancer Treatment Finder", page_icon="🐾")
-st.title("TEST")
-st.write("Streamlit deployment is running.")
+runpy.run_path("pages/1_Clinical_Trial_Finder.py", run_name="__main__")
