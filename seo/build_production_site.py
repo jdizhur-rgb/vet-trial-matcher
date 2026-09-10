7c048797411030c0b518e02d0bedce11d722e306
jdizhur-rgb <j.dizhur@gmail.com>
2026-09-10 16:18:02 -0400

    Apply SEO enhancements to center pages in production build

--- seo/build_production_site.py
+++ seo/build_production_site.py
@@ -23,10 +23,12 @@ generate_seo.SITE = SITE
 import generate_seo_strict
 import generate_cancer_coverage
 import ensure_center_images
+from center_page_enhancements import enhance_center_pages
 
 
 def main():
     generate_seo_strict.main()
+    enhance_center_pages(SEO_DIR / "site")
     generate_cancer_coverage.main()
     ensure_center_images.main()
     out = SEO_DIR / "site"
