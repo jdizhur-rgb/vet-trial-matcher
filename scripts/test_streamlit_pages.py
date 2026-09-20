48333462f3952ef20b7f831a3c47a70b4467e45b
jdizhur-rgb <j.dizhur@gmail.com>
2026-09-20 16:44:00 -0700

    Restore Streamlit navigation test

--- scripts/test_streamlit_pages.py
+++ scripts/test_streamlit_pages.py
@@ -12,16 +12,9 @@ class StreamlitPageSmokeTests(unittest.TestCase):
     def load_oa(self):
         return AppTest.from_file(ROOT / "pages" / "4_Osteoarthritis_Trial_Finder.py", default_timeout=30).run()
 
-    def test_legacy_streamlit_app_redirects_to_current_matcher(self):
+    def test_shared_navigation_loads(self):
         at = AppTest.from_file(ROOT / "app.py", default_timeout=30).run()
         self.assertFalse(at.exception)
-        self.assertTrue(
-            any("https://vettrialfinder.com/matcher/" in item.value for item in at.markdown)
-        )
-        self.assertEqual(
-            at.get("link_button")[0].url,
-            "https://vettrialfinder.com/matcher/",
-        )
 
     def test_cancer_search_still_runs(self):
         at = AppTest.from_file(ROOT / "pages" / "1_Clinical_Trial_Finder.py", default_timeout=30).run()
